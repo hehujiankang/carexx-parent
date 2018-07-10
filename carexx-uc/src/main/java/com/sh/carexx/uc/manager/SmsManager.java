bed8f02fdaecaf8b58c490ffbfcc62cde0ae3e7e
wanglong <wanglong@carexx.cn>
2018-07-11 00:26:09 +0800

    短信

--- carexx-uc/src/main/java/com/sh/carexx/uc/manager/SmsManager.java
+++ carexx-uc/src/main/java/com/sh/carexx/uc/manager/SmsManager.java
@@ -18,27 +18,56 @@ import com.sh.carexx.common.exception.BizException;
 import com.sh.carexx.common.util.HttpClientUtils;
 import com.sh.carexx.common.util.JSONUtils;
 
+/**
+ * 
+ * ClassName: SmsManager <br/> 
+ * Function: 短信发送管理器 <br/> 
+ * Reason: TODO ADD REASON(可选). <br/> 
+ * Date: 2018年7月4日 上午10:31:53 <br/> 
+ * 
+ * @author WL 
+ * @since JDK 1.8
+ */
 @Service
 public class SmsManager {
+	/** 验证码短信 */
 	private static final String SMS_TYPE_VERIFY_CODE = "1";
+	/** 通知短信 */
 	private static final String SMS_TYPE_NOTITY = "2";
+	/** 内容类型 */
 	private static final String SMS_CONTENT_TYPE = "1";
+	/** 验证码有效期（分钟） */
 	private static final int SMS_VERIFY_CODE_EXPIRE = 5;
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-
+	
+	//发送地址
 	@Value("${sms.gateway.serviceUrl}")
 	private String serviceUrl;
+	//用户ID
 	@Value("${sms.user.userId}")
 	private String userId;
+	//密钥
 	@Value("${sms.user.password}")
 	private String password;
+	//默认短信签名
 	@Value("${sms.content.signature}")
 	private String signature;
 
 	@Autowired
 	private RedisTemplate<String, String> redisTemplate;
 
+	/**
+	 * 
+	 * send:发送短信 <br/> 
+	 * @author WL 
+	 * @param busiType
+	 * @param mobiles（多个手机号用英文逗号隔开）
+	 * @param content
+	 * @param signature
+	 * @throws BizException 
+	 * @since JDK 1.8
+	 */
 	public void send(String busiType, String mobiles, String content, String signature) throws BizException {
 		Map<String, String> params = new HashMap<String, String>();
 		params.put("userId", this.userId);
@@ -63,10 +92,28 @@ public class SmsManager {
 		}
 	}
 
+	/**
+	 * 
+	 * sendNotify:发送通知短信 <br/> 
+	 * @author WL 
+	 * @param mobiles
+	 * @param content
+	 * @param signature
+	 * @throws BizException 
+	 * @since JDK 1.8
+	 */
 	public void sendNotify(String mobiles, String content, String signature) throws BizException {
 		this.send(SMS_TYPE_NOTITY, mobiles, content, signature);
 	}
 
+	/**
+	 * 
+	 * sendVerifyCode:发送验证码 <br/> 
+	 * @author WL 
+	 * @param mobile
+	 * @throws BizException 
+	 * @since JDK 1.8
+	 */
 	public void sendVerifyCode(String mobile) throws BizException {
 		String verifyCode = String.valueOf((int) ((Math.random() * 9 + 1) * 100000));
 		this.logger.info("验证码[{}]已成功发送至手机[{}]", verifyCode, mobile);
@@ -76,6 +123,15 @@ public class SmsManager {
 				SMS_VERIFY_CODE_EXPIRE * 60, TimeUnit.SECONDS);
 	}
 
+	/**
+	 * 
+	 * checkSmsVerifyCode:检查短信验证码 <br/> 
+	 * @author WL 
+	 * @param mobile
+	 * @param verifyCode
+	 * @throws BizException 
+	 * @since JDK 1.8
+	 */
 	public void checkSmsVerifyCode(String mobile, String verifyCode) throws BizException {
 		String storedVerifyCode = this.redisTemplate.opsForValue()
 				.get(CarexxConstant.CachePrefix.CAREXX_SMS_VERIFY_CODE + mobile);

46705862d714d4c0a3a806e096dc6ccfa2f2fb94
wanglong <wanglong@carexx.cn>
2018-07-14 14:08:13 +0800

    修复bug

--- carexx-common/src/main/java/com/sh/carexx/common/util/JWTUtils.java
+++ carexx-common/src/main/java/com/sh/carexx/common/util/JWTUtils.java
@@ -1,43 +1,88 @@
 package com.sh.carexx.common.util;
 
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
+public final class JwtUtils {
+    private final static Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtBuilder;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+    private final static long expiredTime = 60 * 60;
 
-public class JWTUtils {
-	private static final String DEFAULT_SUBJECT = "auth";
-	private static final String KEY_ALG = "AES";
+    private final static String key = "secret";
 
-	private static SecretKey generalKey(String secretKey) {
-		byte[] secretKeyBytes = Base64Utils.decode2Bytes(secretKey);
-		SecretKey key = new SecretKeySpec(secretKeyBytes, 0, secretKeyBytes.length, KEY_ALG);
-		return key;
-	}
+    private JwtUtils() {
 
-	public static String createJWT(String id, int ttlSeconds, String secretKey) {
-		SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
-		long nowMillis = System.currentTimeMillis();
-		Date now = new Date(nowMillis);
-		SecretKey key = generalKey(secretKey);
-		JwtBuilder builder = Jwts.builder().setId(id).setSubject(DEFAULT_SUBJECT).setIssuedAt(now)
-				.signWith(signatureAlgorithm, key);
-		if (ttlSeconds >= 0) {
-			long expMillis = nowMillis + ttlSeconds * 1000;
-			Date expDate = new Date(expMillis);
-			builder.setExpiration(expDate);
-		}
-		return builder.compact();
-	}
+    }
 
-	public static String parseJWT(String jwt, String secretKey) throws Exception {
-		SecretKey key = generalKey(secretKey);
-		Claims claims = Jwts.parser().setSigningKey(key).parseClaimsJws(jwt).getBody();
-		return claims.getId();
-	}
+    public static String getToken(ClaimInfo claimInfo) throws Exception {
+        Map<String, Object> header = new HashMap<>(2);
+        header.put("alg", "HS256");
+        header.put("typ", "JWT");
+        long iat = System.currentTimeMillis();
+        long exp = iat + expiredTime * 1000L;
+        String token = JWT.create()
+                .withHeader(header)
+                .withIssuedAt(new Date(iat))
+                .withExpiresAt(new Date(exp))
+                .withClaim("userId", claimInfo.getUserId())
+                .withClaim("userName", claimInfo.getUserName())
+                .sign(Algorithm.HMAC256(key));
+        return token;
+    }
+
+    public static Map<String, Claim> verifyToken(String token) {
+        DecodedJWT jwt;
+        try {
+            JWTVerifier verifier = JWT.require(Algorithm.HMAC256(key)).build();
+            jwt = verifier.verify(token);
+            Map<String, Claim> claims = jwt.getClaims();
+            return jwt.getClaims();
+        } catch (Exception ex) {
+            logger.error("Verify jwt token failed, token={}", token, ex);
+            return null;
+        }
+    }
+
+    static class ClaimInfo {
+        private String userId;
+
+        private String userName;
+
+        public String getUserId() {
+            return userId;
+        }
+
+        public void setUserId(String userId) {
+            this.userId = userId;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+    }
+
+    public static void main(String[] args) {
+        JwtUtils.ClaimInfo info = new JwtUtils.ClaimInfo();
+        info.setUserId("1");
+        info.setUserName("1");
+        try {
+            System.out.print(JwtUtils.getToken(info));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }

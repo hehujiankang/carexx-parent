6efe736f4e53048599c0d81872dc0e785ae4e471
wanglong <wanglong@carexx.cn>
2018-07-16 10:00:19 +0800

    修复bug

--- carexx-uc/src/main/java/com/sh/carexx/uc/controller/UserController.java
+++ carexx-uc/src/main/java/com/sh/carexx/uc/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.sh.carexx.uc.controller;
 
+import com.sh.carexx.common.web.BasicRetVal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +13,16 @@ import com.sh.carexx.uc.service.UserService;
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@RequestMapping(value = "/get_user_info/{id}", method = RequestMethod.GET)
-	public UserInfo getUserInfo(@PathVariable("id") Integer id) {
-		return this.userService.getById(id);
-	}
+    @RequestMapping(value = "/get_user_info/{id}", method = RequestMethod.GET)
+    public UserInfo getUserInfo(@PathVariable("id") Integer id) {
+        return this.userService.getById(id);
+    }
+
+    @RequestMapping(value = "/modify_bind_mobile/{mobile}/{verifyCode}", method = RequestMethod.GET)
+    public BasicRetVal modifyBindMobile(@PathVariable("mobile") String mobile, @PathVariable("verifyCode") String verifyCode) {
+        return null;
+    }
 }
